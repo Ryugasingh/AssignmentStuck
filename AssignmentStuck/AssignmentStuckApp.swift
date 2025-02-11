7064635c79e7778e054a754673cd433bfdc69ce6
Sambhav Singh <ryugasingh90@gmail.com>
2025-02-11 13:57:43 +0530

    Initial Commit

---
+++ AssignmentStuck/AssignmentStuckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AssignmentStuckApp.swift
+//  AssignmentStuck
+//
+//  Created by Sambhav Singh on 11/02/25.
+//
+
+import SwiftUI
+
+@main
+struct AssignmentStuckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
